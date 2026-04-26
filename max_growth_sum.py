7923c491f8c2e6a695a98858f115fcb86f03833f
Max Miroshnichenko <foxmax@list.ru>
2026-04-26 17:39:13 +0300

    Обработка 0 внутри чисел

--- max_growth_sum.py
+++ max_growth_sum.py
@@ -4,38 +4,44 @@ numbers.sort(reverse=True)
 
 size_max_digit = len(str(numbers[0]))
 temp_list = []
+data = {}
 result = 0
 
 while True:
     while True:
-        if len(str(numbers[0])) < size_max_digit or len(str(numbers[0])) == 1:
+        if not numbers or len(str(numbers[0])) < size_max_digit or len(str(numbers[0])) == 1:
             break
-        num = numbers.pop(0)
-        f_digit = num // 10 ** (size_max_digit - 1)
-        tail_num = num % 10 ** (size_max_digit - 1)
-        if f_digit != 9:
-            temp_list.append(f_digit)
-        numbers.append(tail_num)
+        num = str(numbers.pop(0))
+        for i in range(len(num)):
+            key = len(num) - 1 - i
+            value = int(num[i])
+            if value != 9:
+                data.setdefault(key, []).append(value)
 
     if size_max_digit == 1:
         for num in numbers:
             if num != 9:
-                temp_list.append(n)
+                data.setdefault(0, []).append(num)
         numbers = []
 
-    temp_list.sort()
+    temp_list = sorted(data.get(size_max_digit - 1, []))
     for num in temp_list:
         result += (9 - num) * 10 ** (size_max_digit - 1)
         k -= 1
         if k <= 0:
             break
 
-    if k == 0 or not numbers:
+    if k == 0 or (not numbers and size_max_digit == 1):
         break
 
     if k > 0:
-        numbers.sort(reverse=True)
-        size_max_digit = len(str(numbers[0]))
-        temp_list = []
+        size_max_digit -= 1
 
 print(result)
+
+
+# Алгоритм кратко:
+#
+# Берем самые большие числа и делим их по разрядам, сохраняя в словать data. Если число равно 9, то не добавляем
+# Выбираем получившийся списо максимального разряда, соритруем, и по очереди получаем разницу увеличения.
+# Если после этого k>0, то повторяем процесс со следующим разрядом
